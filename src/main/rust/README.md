6b9d111780825cc6df56614576ce59ad25c2b733
Pranav Bijapur <bijapurpranav@gmail.com>
2025-04-19 18:06:13 -0700

    add test code, first 2 samples passing

--- src/main/rust/README.md
+++ src/main/rust/README.md
@@ -4,8 +4,14 @@
 * Generate `measurements.txt` 
 * [Install Rust](https://www.rust-lang.org/tools/install)
 
+### Release run
+* `cargo build --release && ./target/release/calculate_average_pranavsb`
+
 ### Debugging
 * `cargo build && ./target/debug/calculate_average_pranavsb`
 
-### Release run
-* `cargo build --release && ./target/release/calculate_average_pranavsb`
+### Testing
+* to run a particular test from `test/resources/samples`:
+    * `cargo build && ./target/debug/calculate_average_pranavsb ../../test/resources/samples/measurements-3.txt`
+
+### Benchmarks
--- src/main/rust/src/calculate_average_pranavsb.rs
+++ src/main/rust/src/calculate_average_pranavsb.rs
@@ -1,12 +1,21 @@
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
+use std::path::Path;
 
 macro_rules! debug_println {
     ($($arg:tt)*) => {
         #[cfg(debug_assertions)] {
             println!($($arg)*);
+        }
+    };
+}
+
+macro_rules! debug_validate {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)] {
+            debug_validate($($arg)*);
         }
     };
 }
@@ -35,11 +44,17 @@ fn main() {
     debug_println!("Max temps: {:?}", max_temps);
     debug_println!("Avg temps: {:?}", avg_temps);
 
-    let outfile = filename.split("/").last().expect("badly formatted filename");
-    write_output_file(outfile, avg_temps, min_temps, max_temps);
+    let outfile = get_output_filename(filename);
+    debug_println!("Output file: {}", outfile);
+    write_output_file(&outfile, avg_temps, min_temps, max_temps);
+
+    // validate output if running debug build
+    debug_validate!(&outfile);
 }
 
-fn populate_raw_data(file: File) -> (
+fn populate_raw_data(
+    file: File,
+) -> (
     HashMap<String, f64>,
     HashMap<String, f64>,
     HashMap<String, f64>,
@@ -79,7 +94,10 @@ fn populate_raw_data(file: File) -> (
     return (min_temps, max_temps, sum_temps, count_temps);
 }
 
-fn calculate_average(mut sum_temps: HashMap<String, f64>, count_temps: HashMap<String, f64>) -> HashMap<String, f64> {
+fn calculate_average(
+    mut sum_temps: HashMap<String, f64>,
+    count_temps: HashMap<String, f64>,
+) -> HashMap<String, f64> {
     sum_temps.iter_mut().for_each(|(city, sum)| {
         let count = count_temps.get(city).expect("count not found");
         *sum = *sum / *count;
@@ -87,25 +105,87 @@ fn calculate_average(mut sum_temps: HashMap<String, f64>, count_temps: HashMap<S
     sum_temps
 }
 
-fn write_output_file(filename: &str, avg_temps: HashMap<String, f64>, max_temps: HashMap<String, f64>, min_temps: HashMap<String, f64>) {
+fn write_output_file(
+    filename: &str,
+    avg_temps: HashMap<String, f64>,
+    max_temps: HashMap<String, f64>,
+    min_temps: HashMap<String, f64>,
+) {
     let mut cities = avg_temps.keys().cloned().collect::<Vec<String>>();
     cities.sort();
 
     let mut output_map: HashMap<String, String> = HashMap::new();
     for city in cities.iter() {
-        let val = format!("{:.1}/{:.1}/{:.1}", min_temps.get(city).unwrap(), avg_temps.get(city).unwrap(), max_temps.get(city).unwrap());
+        let val = format!(
+            "{:.1}/{:.1}/{:.1}",
+            min_temps.get(city).unwrap(),
+            avg_temps.get(city).unwrap(),
+            max_temps.get(city).unwrap()
+        );
         output_map.insert(city.to_string(), val);
     }
 
-    let mut outfile = File::create(format!("./out/{}", filename)).expect("could not create output file");
-    outfile.write_all("{".as_bytes()).expect("could not write to output file");
-    for (i, (city, val)) in output_map.iter().enumerate() {
+    let mut outfile =
+        File::create(format!("./out/{}", filename)).expect("could not create output file");
+    outfile
+        .write_all("{".as_bytes())
+        .expect("could not write to output file");
+    for (i, (city, val)) in cities.iter().zip(output_map.values()).enumerate() {
         if i == cities.len() - 1 {
-            outfile.write_all(format!("{}={}", city, val).as_bytes()).expect("could not write to output file");
+            outfile
+                .write_all(format!("{}={}", city, val).as_bytes())
+                .expect("could not write to output file");
             break;
         }
-        outfile.write_all(format!("{}={}, ", city, val).as_bytes()).expect("could not write to output file");
+        outfile
+            .write_all(format!("{}={}, ", city, val).as_bytes())
+            .expect("could not write to output file");
     }
-    outfile.write_all("}".as_bytes()).expect("could not write to output file");
+    outfile
+        .write_all("}\n".as_bytes())
+        .expect("could not write to output file");
 }
 
+// not doing anything fancy to avoid dependencies
+// no need to show diff, we can manually compare both files
+fn debug_validate(outfile: &str) {
+    let mut expected_reader = BufReader::new(
+        File::open(format!("../../test/resources/samples/{}", outfile))
+            .expect("could not open expected output"),
+    );
+    let mut actual_reader = BufReader::new(
+        File::open(format!("./out/{}", outfile)).expect("could not open output file"),
+    );
+
+    let mut buf_expected = [0; 1000];
+    let mut buf_actual = [0; 1000];
+    loop {
+        let n_expected = expected_reader
+            .read(&mut buf_expected)
+            .expect("could not read expected output");
+        let n_actual = actual_reader
+            .read(&mut buf_actual)
+            .expect("could not read output file");
+
+        if n_expected == 0 && n_actual == 0 {
+            println!("Output file matches expected output");
+            break;
+        } else if n_expected != n_actual {
+            panic!("Output file does not match expected output");
+        }
+        if buf_expected == buf_actual {
+            continue;
+        }
+        panic!("Output file does not match expected output");
+    }
+}
+
+fn get_output_filename(filename: &str) -> String {
+    let path = Path::new(filename);
+    path.file_name()
+        .and_then(|name| name.to_str())
+        .and_then(|name| name.split('.').next())
+        .and_then(|name| Some(format!("{}.out", name)))
+        .unwrap()
+        .to_string()
+}
